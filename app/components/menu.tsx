3cd862f77aae857e154000b5bd12fe627cd24736
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-03-03 16:09:13 +0100

    user panel

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -153,7 +153,7 @@ const Menu = () => {
 							className='bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium'
 							aria-current='page'
 						>
-							Zaloguj się
+							Zaloguj się!
 						</button>
 					</Link>
 				</div>
---
+++ app/userpanel/page.tsx
@@ -0,0 +1,9 @@
+export default function UserPanel() {
+	return (
+		<>
+			<div>
+				<p>User Panel</p>
+			</div>
+		</>
+	);
+}
