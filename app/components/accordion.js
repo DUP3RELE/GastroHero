3823e46302346fff7b294b5b71f3de1f4e1127d8
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-03-10 20:39:38 +0100

    docs update

--- app/components/accordion.js
+++ app/components/accordion.js
@@ -7,7 +7,7 @@ const Accordion = ({ title, children }) => {
 	return (
 		<div className='w-full'>
 			<button
-				className='w-full text-left px-4 py-2 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:bg-gray-300'
+				className='w-3/4 text-left m-2 px-4 py-2 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 text-black'
 				onClick={() => setIsOpen(!isOpen)}
 			>
 				{title}
--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -4,8 +4,18 @@ const Docs = () => {
 	return (
 		<div className='container mx-auto px-4'>
 			<h1 className='text-2xl font-bold my-4'>Dokumentacja</h1>
-			<Accordion title='Tytuł Dokumentu 1'>Treść dokumentu 1...</Accordion>
-			<Accordion title='Tytuł Dokumentu 2'>Treść dokumentu 2...</Accordion>
+			<Accordion title='1. Główne założenia aplikacji'>
+				<p className='w-3/4'>
+					Aplikacja GastroHero służy do szerokopojętego zarządzania restauracją.
+					Użytkownik najpierw tworzy profil restauracji, w której rejestruje pracowników. Główny użytkownik może w profilu restauracji tworzyć konta pracowników, nadawać im uprawnienia (np.: kucharz ma wgląd do receptur i mepa, a kelner ma wgląd do rezerwacji, zamówień na sali, stanu dań - gdy czegoś brakuje - itp.). Aplikacja posiada czaty pracownicze, które również mogą być dzielone na grupy pracowników. 
+				</p>
+			</Accordion>
+			<Accordion title='Tytuł Dokumentu 2'>
+				<p className='w-3/4'>
+					zamówienia, rezerwację, czat pracowniczy, statystyki dań, godziny
+					pracowników, receptury, mep pracowniczy, stany magazynowe.
+				</p>
+			</Accordion>
 		</div>
 	);
 };
