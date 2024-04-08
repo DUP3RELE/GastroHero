622f62ca9394aa2a54b4022b203ec764a92b7009
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-04-08 18:51:05 +0200

    work planning day, notion created

---
+++ app/pages/userpanel/employeePanel/page.tsx
@@ -0,0 +1,10 @@
+export default function employeePanel() {
+	return (
+		<>
+			<div><p>Hello (name)</p></div>
+            <div><p>chat</p></div>
+            <div><p>hours</p></div>
+            <div><p>additional data</p></div>
+		</>
+	);
+}
