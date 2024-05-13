0e1b6584ce7c0fda36d0fb9571914fce4dd5cf8f
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-05-13 21:42:19 +0200

    working on recipies

---
+++ app/components/datablocks/recipies.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function RecipiesData() {
+	return (
+		<>
+			<div className='w-1/4 m-3 p-3 rounded border shadow-lg dark:shadow-blue-800 shadow-blue-300'>
+				<div className='m-3'>
+					<p>Lista Receptur</p>
+				</div>
+				<div className='m-2'>
+					<ul>
+						<li>Danie nr. 1</li>
+						<li>Danie nr. 2</li>
+						<li>Danie nr. 3</li>
+						<li>Danie nr. 4</li>
+						<li>Danie nr. 5</li>
+						<li>...</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
+}
