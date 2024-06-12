bbc29a5dab3869bda9516f107e7db33425553ae7
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-06-12 09:22:31 +0200

    working on employee login

--- app/api/login.tsx
+++ app/api/login.tsx
@@ -3,7 +3,9 @@ import { useAuth } from "./hooks/useAuthToken";
 
 interface LoginResponse {
 	access_token: string;
-	restaurant_id: number;
+	restaurant_id?: number;
+	employee_id?: number;
+	userType: "restaurant" | "employee";
 }
 
 export const useLogin = () => {
@@ -23,10 +25,18 @@ export const useLogin = () => {
 			if (response.ok) {
 				const data: LoginResponse = await response.json();
 				console.log("Login successful:", data);
+
 				localStorage.setItem("token", data.access_token);
-				localStorage.setItem("restaurant_id", data.restaurant_id.toString());
+
+				if (data.userType === "restaurant") {
+					localStorage.setItem("restaurant_id", data.restaurant_id!.toString());
+					router.push("../userpanel");
+				} else if (data.userType === "employee") {
+					localStorage.setItem("employee_id", data.employee_id!.toString());
+					router.push("../userpanel/employeePanel");
+				}
+
 				loginAction();
-				router.push("../userpanel");
 			} else {
 				const errorData = await response.json();
 				throw new Error(errorData.message || "Nieznany błąd logowania.");
