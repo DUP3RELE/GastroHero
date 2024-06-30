6cb2841d6f604a798f37db44048e8f9da7ece551
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-06-30 21:10:37 +0200

    ble

--- app/pages/hourManager/page.tsx
+++ app/pages/hourManager/page.tsx
@@ -1,5 +1,6 @@
 import { useEmployeeName } from "@/app/api/hooks/useEmployeeName";
 
 export default function hourManager() {
-	return <></>;
+	return <><div><p>Hello (name), your hour count in month (month) is: (hours)</p></div>
+	<div><p>months, days</p></div></>;
 }
